ee1a984bff18473e58411a5b316bf7021cc92330
THANUJ KUMAR S C <thanuj_tk@yahoo.com>
2021-07-24 13:56:13 +0530

    minor changes

--- data-access/src/test/java/org/tk/spring/jpa/StockJpaRepositoryTest.java
+++ data-access/src/test/java/org/tk/spring/jpa/StockJpaRepositoryTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {JpaJavaConfig.class})
+@SpringJUnitConfig( {JpaJavaConfig.class}) //Below two annotations not required
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = {JpaJavaConfig.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StockJpaRepositoryTest {
 
